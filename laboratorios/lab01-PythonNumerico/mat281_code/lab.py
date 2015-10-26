cfe0c7e3dd34257520a0b814bf9318d95b2bd973
Sebastian Flores <sebastiandres@gmail.com>
2015-10-26 17:09:07 -0300

    lab python numerico v1

---
+++ laboratorios/lab01-PythonNumerico/mat281_code/lab.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from sols import *
+
+def greetings(st_1, st_2):
+    template = "<b>Alumno</b>: {0:30} <b>rol</b>: {1:10}<br>"
+    hi  = ""
+    hi += template.format(*st_1)
+    hi += template.format(*st_2)
+    ending = "Good luck!"
+    return hi + ending
