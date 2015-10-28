5d723a1d760c705a3082a52abe249d813f4c2cb2
Sebastian Flores <sebastiandres@gmail.com>
2015-10-27 23:26:05 -0300

    Lab python numerico

--- laboratorios/lab01-PythonNumerico/mat281_code/lab.py
+++ laboratorios/lab01-PythonNumerico/mat281_code/lab.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from sols import *
 
 def greetings(st_1, st_2):
     template = "<b>Alumno</b>: {0:30} <b>rol</b>: {1:10}<br>"
     hi  = ""
     hi += template.format(*st_1)
     hi += template.format(*st_2)
-    ending = "Good luck!"
-    return hi + ending
+    warning = "</br>Lea el notebook y comente con su compañero de laboratorio."
+    warning = "</br>Este es un laboratorio práctico y de aprendizaje:"
+    warning+= "</br>Tome apuntes, intente lo sugerido y pruebe otros cambios."
+    ending = "</br></br><b>¡Buena suerte!</b>"
+    return hi + warning + ending
